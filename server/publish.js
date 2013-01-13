080d3fe13a45f1b969d9b6f932403fbb64ef692d
Ubuntu <tim@ford.(none)>
2013-01-13 12:52:38 +0000

    user finish

--- server/publish.js
+++ server/publish.js
@@ -1,7 +1,21 @@
 Meteor.publish('admin', function() {
   return Meteor.users.find({}, {sort: {createdAt: 1}, fields: {'_id': 1, 'profile': 1, 'createdAt': 1}});
 });
+
+var adminId = Meteor.users.findOne({}, {sort: {createdAt: 1}, fields: {'_id': 1, 'profile': 1, 'createdAt': 1}})._id;
+
 Articles = new Meteor.Collection('articles');
+Articles.allow({
+  insert: function (userId, doc) {
+    return (userId === adminId);
+  },
+  update: function (userId, docs, fields, modifier) {
+    return (userId === adminId);
+  },
+  remove: function (userId, docs) {
+    return (userId === adminId);
+  }
+});
 Meteor.publish('articles', function () {
   return Articles.find({});
 });
